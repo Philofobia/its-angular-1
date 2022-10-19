4d855e18545b6abfe55a9b890e112e5c0656e088
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2022-10-19 10:22:45 +0200

    searchBar made

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,20 +5,29 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
-  drinks = [];
-  constructor(private httpClient: HttpClient) { }
-  
+  drinks:any[] = [];
+  searchDrinks:any[] = [];
+  constructor(private httpClient: HttpClient) {}
+
+  jsonIn = {
+    searchInput: '',
+  };
+
+  handleSearch(): void {
+    this.httpClient
+      .get(
+        `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.jsonIn.searchInput}`
+      )
+      .subscribe((response: any) => {
+        this.searchDrinks = response.drinks;
+      });
+  }
+
   ngOnInit(): void {
     this.httpClient
-    .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
-    .subscribe((response: any) => {
-      this.drinks = response.drinks;
-    })
+      .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
+      .subscribe((response: any) => {
+        this.drinks = response.drinks;
+      });
   }
-}
-
-interface drink {
-  idDrink: string,
-  nameDrink: string,
-  imgDrink: string,
 }
